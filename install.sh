7a70189e63a5274db4e31b5fdc1f37e09e6d87b8
Francesco Poldi <francesco.poldi@protonmail.ch>
2019-01-23 22:13:30 +0100

    Updated

--- install.sh
+++ install.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 # Installation script for Debian based systems
 
-# Install Dependencies 
+# Install Dependencies
 sudo apt-get install -y libsqlite3-dev \
   libxss1 \
   libx11-xcb-dev \
   libxtst-dev \
-  libgconf-2-4 \ 
-  libnss3 \ 
+  libgconf-2-4 \
+  libnss3 \
   libasound-dev
-  
+
 # Install node-sqlite3
-git clone git clone https://github.com/mapbox/node-sqlite3.git
+git clone https://github.com/mapbox/node-sqlite3.git
 cd node-sqlite3
 npm install --build-from-source
 
